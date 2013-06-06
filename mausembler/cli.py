f67575a97c4e8877c938ae0374dd36669915d287
Dominic May <jack.thatch@gmail.com>
2013-06-06 22:28:51 +0800

    Oh, and this stuff

---
+++ mausembler/cli.py
@@ -0,0 +1,41 @@
+import os
+from .assembler import Assembler
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_filename")
+    parser.add_argument("output_filename")
+    parser.add_argument("--credits", help="prints credits and exits")
+    parser.add_argument("--big-endian", help="enables big endian")
+    parser.add_argument("--little-endian", help="enables little endian")
+    parser.add_argument(
+        "-d", "--debug", help="enable debug mode", action="store_true")
+    args = parser.parse_args()
+
+    if args.credits:
+        print('First, notch! Cheers matey!')
+        print('Secondly, startling! For answering my questions!')
+        print('And thirdly, me. For writing the code')
+    else:
+        input_filename = os.path.abspath(args.input_filename)
+        output_filename = os.path.abspath(args.output_filename)
+
+        asm = Assembler(
+            {'input_directory': os.path.abspath(
+                os.path.dirname(input_filename))})
+
+        asm.debug_toggle = args.debug
+        asm.endian = 'big' if args.big_endian else 'little'
+
+        with open(input_filename) as file_handle:
+            assembly = file_handle.readlines()
+
+        assembly = asm.assemble(assembly)
+
+        output_filename
+
+
+if __name__ == '__main__':
+    main()
---
+++ mausembler/representations.py
@@ -0,0 +1,114 @@
+import os
+import re
+
+
+class ReferenceRep(object):
+    pass
+
+
+class Expression(object):
+    def __init__(self, expr):
+        self.HEX_RE = re.compile(r'(0x\d+)')
+        self.BIN_RE = re.compile(r'(0b[01]+)')
+
+    def resolve(self):
+        return 'new_assembly'
+
+
+class Rep(object):
+    def __init__(self, assembler_ref, **kwargs):
+        self.assembler_ref = assembler_ref
+        self.attrs = kwargs
+
+    def size(self):
+        raise NotImplementedError()
+
+    def hexlify(self):
+        # this doesn't strictly have to return anything
+        return None
+
+    def resolve(self, state):
+        # this is used for special actions, such as opcodes or instructions
+        # that expand out into more opcodes
+        return None, None
+
+
+class CommentRep(Rep):
+    def __repr__(self):
+        return "<Comment: {}>".format(self.attrs['content'])
+
+
+class LabelRep(Rep):
+    def size(self):
+        return 0
+
+    def __repr__(self):
+        return "<Label: {}>".format(self.attrs['name'])
+
+
+class BasicOpcodeRep(Rep):
+    contains_reference = False
+
+    def __repr__(self):
+        return "<BasicOpcope: {} {} {}>".format(
+            self.attrs['name'],
+            self.attrs['frag_b'],
+            self.attrs['frag_a'])
+
+    def hexlify(self):
+        assert not self.contains_reference
+        return self.assembler_ref.assemble_opcode(self)
+
+
+class SpecialOpcodeRep(Rep):
+    def __repr__(self):
+        return "<SpecialOpcode: {} {}>".format(
+            self.attrs['name'],
+            self.attrs['frag_a'])
+
+
+class DirectiveRep(Rep):
+    def size(self):
+        return 0
+
+    def __repr__(self):
+        extra_params = (
+            self.attrs['extra_params'] if self.attrs['extra_params']
+            else "{}")
+        return "<{} {}>".format(
+            self.attrs['name'],
+            extra_params)
+
+
+class IncludeDirectiveRep(DirectiveRep):
+    def resolve(self, state):
+        "Loads an included file"
+
+        filename = self.attrs['extra_params'].rstrip()
+        if not os.path.exists(filename):
+            filename = os.path.join(state['input_directory'], filename)
+
+        try:
+            with open(filename) as dep_handler:
+                return 'new_assembly', dep_handler.readlines()
+        except FileNotFoundError as e:
+            raise FileNotFoundError(
+                'Loading include failed with; {}'.format(e))
+
+
+class DataLiteralRep(DirectiveRep):
+    def __repr__(self):
+        return "<DataLit: {}>".format(self.attrs['content'])
+
+    def size(self):
+        raise NotImplementedError()
+
+    def hexlify(self):
+        content = self.attrs['content']
+
+        if content.rstrip()[0:2] == '0x':
+            return int(content, 16)
+        elif content.rstrip()[0:2] == '0b':
+            return int(content, 2)
+        else:
+            return content
